a3dda84213d9d74e5f7fa278bb505f1bf29ebf1f
Nikavocado <77246165+Nikavocado@users.noreply.github.com>
2021-02-25 18:57:51 -0600

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y, radius){
+    var options = {
+    isStatic: false,
+    'restitution':0.3,
+    'friction':0.5,
+    'density':3.2
+    }
+    
+    this.radius = radius
+    this.body = Bodies.circle(x,y,this.radius/2,options);
+    
+    World.add(world,this.body);
+    }
+    display(){
+        push();
+translate(this.body.position.x,this.body.position.y)
+    ellipseMode(RADIUS)
+    fill("yellow");
+    ellipse(0,0,this.radius,this.radius);
+    pop();
+    }
+    
+    };
